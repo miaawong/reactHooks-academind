5af37abeb7ca44d007e18a566010f5ee91f2cb73
Mia Wong <miaw629@gmail.com>
2019-08-10 21:16:36 -0400

    useEffect instead of componentDidUpdate, set 2nd argument of useEffect to be an array of dependencies ( of a variable that you want to listen for, to update)... also useEffect can return a function, which will run just before useEffect is run again === componentWillUnmount

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,85 +1,99 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Summary from './Summary';
+import Summary from "./Summary";
 
-class Character extends Component {
-  state = { loadedCharacter: {}, isLoading: false };
+const Character = props => {
+    const [loadedCharacter, setLoadedCharacter] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return (
-      nextProps.selectedChar !== this.props.selectedChar ||
-      nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
-      nextState.isLoading !== this.state.isLoading
-    );
-  }
+    const fetchData = () => {
+        console.log(
+            "Sending Http request for new character with id " +
+                props.selectedChar
+        );
+        setIsLoading(true);
+        fetch("https://swapi.co/api/people/" + props.selectedChar)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Could not fetch person!");
+                }
+                return response.json();
+            })
+            .then(charData => {
+                const loadedCharacter = {
+                    id: props.selectedChar,
+                    name: charData.name,
+                    height: charData.height,
+                    colors: {
+                        hair: charData.hair_color,
+                        skin: charData.skin_color
+                    },
+                    gender: charData.gender,
+                    movieCount: charData.films.length
+                };
+                setLoadedCharacter(loadedCharacter);
+                setIsLoading(false);
+            })
+            .catch(err => {
+                console.log(err);
+                setIsLoading(false);
+            });
+    };
+    // shouldComponentUpdate(nextProps, nextState) {
+    //   console.log('shouldComponentUpdate');
+    //   return (
+    //     nextProps.selectedChar !== this.props.selectedChar ||
+    //     nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
+    //     nextState.isLoading !== this.state.isLoading
+    //   );
+    // }
 
-  componentDidUpdate(prevProps) {
-    console.log('Component did update');
-    if (prevProps.selectedChar !== this.props.selectedChar) {
-      this.fetchData();
-    }
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = () => {
-    console.log(
-      'Sending Http request for new character with id ' +
-        this.props.selectedChar
-    );
-    this.setState({ isLoading: true });
-    fetch('https://swapi.co/api/people/' + this.props.selectedChar)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Could not fetch person!');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const loadedCharacter = {
-          id: this.props.selectedChar,
-          name: charData.name,
-          height: charData.height,
-          colors: {
-            hair: charData.hair_color,
-            skin: charData.skin_color
-          },
-          gender: charData.gender,
-          movieCount: charData.films.length
+    // componentDidUpdate(prevProps) {
+    //   console.log('Component did update');
+    //   if (prevProps.selectedChar !== this.props.selectedChar) {
+    //     this.fetchData();
+    //   }
+    // }
+    //componentDidUpDate, set your dependencies to the variable that you want it to listen for, it will update that when it is changed
+    useEffect(() => {
+        fetchData();
+        // this code will run right before useEffect runs the next time  (componentWillUnmount), a cleanup function
+        return () => {
+            console.log("cleaning up... unmount");
         };
-        this.setState({ loadedCharacter: loadedCharacter, isLoading: false });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+    }, [props.selectedChar]);
 
-  componentWillUnmount() {
-    console.log('Too soon...');
-  }
+    //dont send a redundent http request from useEffect twice
+    // useEffect(() => {
+    //     fetchData();
+    // }, []);
 
-  render() {
+    // componentWillUnmount() {
+    //   console.log('Too soon...');
+    // }
+    useEffect(() => {
+        return () => {
+            console.log("component did unmount");
+        };
+    }, []);
+
     let content = <p>Loading Character...</p>;
 
-    if (!this.state.isLoading && this.state.loadedCharacter.id) {
-      content = (
-        <Summary
-          name={this.state.loadedCharacter.name}
-          gender={this.state.loadedCharacter.gender}
-          height={this.state.loadedCharacter.height}
-          hairColor={this.state.loadedCharacter.colors.hair}
-          skinColor={this.state.loadedCharacter.colors.skin}
-          movieCount={this.state.loadedCharacter.movieCount}
-        />
-      );
-    } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
-      content = <p>Failed to fetch character.</p>;
+    if (!isLoading && loadedCharacter.id) {
+        content = (
+            <Summary
+                name={loadedCharacter.name}
+                gender={loadedCharacter.gender}
+                height={loadedCharacter.height}
+                hairColor={loadedCharacter.colors.hair}
+                skinColor={loadedCharacter.colors.skin}
+                movieCount={loadedCharacter.movieCount}
+            />
+        );
+    } else if (!isLoading && !loadedCharacter.id) {
+        content = <p>Failed to fetch character.</p>;
     }
     return content;
-  }
-}
+};
 
 export default Character;
